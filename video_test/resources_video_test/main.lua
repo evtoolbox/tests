5da7bba87c7944b3b4aaaeb8094f973210913910
Igor Ivanov <laptabrok@gmail.com>
2019-06-30 17:41:57 +0300

    [+] Video test.

---
+++ video_test/resources_video_test/main.lua
@@ -0,0 +1,75 @@
+EV.Logger.setCatPriority("ev_osg.EVosgAV", "DEBUG")
+
+
+CFG_HISTORY_SIZE		= 8
+CFG_TEST_PERIOD			= 5.0	-- seconds
+
+local video				= reactorController:getReactorByName("video")
+local btnTest			= reactorController:getReactorByName("btn_test")
+local timer				= reactorController:getReactorByName("timer")
+
+local tests = {
+	{ "play" },
+	{ "pause" },
+	{ "rewind", "play" },
+	{ "stop", "hide" },
+	{ "play", "show" },
+}
+local currentTest = 1
+
+timer:subscribeEvent("onAlarm", function()
+	loginfo("test", currentTest, "of", #tests)
+	for _, action in ipairs(tests[currentTest]) do
+		loginfo("\tperform action", action)
+		video[action](video)
+	end
+	currentTest = currentTest + 1
+
+	if currentTest > #tests then timer:reset() end
+end)
+
+btnTest:subscribeEvent("onDown", function()
+	timer:reset()
+	currentTest = 1
+	timer:start(CFG_TEST_PERIOD, TimerReactor.Mode.LOOP)
+end)
+
+--------------------------------------------------------------------------------
+
+local lblLastEvent		= reactorController:getReactorByName("lbl_last_event")
+local lblEventsHistory	= reactorController:getReactorByName("lbl_events_history")
+
+lblLastEvent.text.value			= ""
+lblEventsHistory.text.value		= ""
+
+local eventsHistory = {}
+local events = {
+	"onPlay",
+	"onPause",
+	"onFinish",
+	"onRewind",
+	"onShowed",
+	"onHidden",
+}
+
+function updateLog(eventName)
+	table.insert(eventsHistory, eventName)
+	if #eventsHistory > CFG_HISTORY_SIZE then
+		table.remove(eventsHistory, 1)
+	end
+
+	local text = ""
+	for i = #eventsHistory, 1, -1 do
+		text = text .. eventsHistory[i] .. "\n"
+	end
+
+	-- lblLastEvent.text.value			= eventName
+	lblEventsHistory.text.value		= text
+end
+
+for _, eventName in ipairs(events) do
+	video:subscribeEvent(eventName, function()
+		loginfo("Received event", eventName)
+		updateLog(eventName)
+	end)
+end
